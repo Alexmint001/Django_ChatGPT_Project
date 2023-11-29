5aee64c2f3b4894bfc015f44351fd19284dd728a
Alexmint001 <142385654+Alexmint001@users.noreply.github.com>
2023-11-29 11:00:08 +0900

    💡 CHATBOT 요청 횟수 오류 개선

--- chatbot/throttles.py
+++ chatbot/throttles.py
@@ -2,9 +2,9 @@ from rest_framework.throttling import UserRateThrottle
 
 class ChatBotThrottle(UserRateThrottle):
     rate = '5/day'
-    
+    scope = 'post_user'
     def allow_request(self, request, view):
         if request.method == 'POST':
             return super().allow_request(request, view)
-        return
+        return True
     
